4b7bcb705c1bf4dfdd85f49fa659348a8cbd5e4f
changHyeon <laver94@naver.com>
2023-09-02 18:29:18 +0900

    #3 서비스 단위테스트 완료

--- tddPractice/src/main/java/com/example/tddpractice/service/MemberService.java
+++ tddPractice/src/main/java/com/example/tddpractice/service/MemberService.java
@@ -1,13 +1,17 @@
 package com.example.tddpractice.service;
 
+import com.example.tddpractice.domain.Member;
 import com.example.tddpractice.dto.MemberDto;
 import com.example.tddpractice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
@@ -15,5 +19,14 @@ public class MemberService {
 
   public List<MemberDto> findDtoList() {
     return null;
+  }
+
+  public Member findMemberById(Long memberId) {
+    return memberRepository.findById(memberId).orElse(null);
+  }
+
+  @Transactional
+  public void save(Member mockMember) {
+    memberRepository.save(mockMember);
   }
 }
--- tddPractice/src/test/java/com/example/tddpractice/service/MemberServiceUnitTest.java
+++ tddPractice/src/test/java/com/example/tddpractice/service/MemberServiceUnitTest.java
@@ -1,10 +1,21 @@
 package com.example.tddpractice.service;
 
+import com.example.tddpractice.domain.Member;
+import com.example.tddpractice.dto.MemberDto;
 import com.example.tddpractice.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceUnitTest {
@@ -14,7 +25,34 @@ class MemberServiceUnitTest {
   @Mock
   MemberRepository memberRepository;
 
-  
+  @Test
+  @DisplayName("상세조회")
+  void 상세조회() {
+    Long memberId = 1L;
+    Long notId = 2L;
+    String memberName = "John";
+    Member member = Member.builder().name(memberName).build();
+
+    when(memberRepository.findById(memberId)).thenReturn(Optional.of(member));
+
+    assertThat(memberService.findMemberById(memberId)).isEqualTo(member);
+    assertThat(memberService.findMemberById(memberId).getName()).isEqualTo(memberName);
+
+    assertThat(memberService.findMemberById(notId)).isNull();
+  }
+
+  @Test
+  @DisplayName("멤버 save")
+  void save() {
+    String memberName = "John";
+    Member mockMember = Member.builder().name(memberName).build();
+    memberService.save(mockMember);
+    verify(memberRepository).save(mockMember); //실행되었는지 확인
+  }
+
+
+
+
 
 
 
